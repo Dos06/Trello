7bff191aed452452cbd36a1188101602e385f3d2
Dos06 <dostermorgan815@gmail.com>
2021-02-17 15:08:14 +0600

    Refactor checkbox -> switch

--- src/main/frontend/src/components/CardDetails/Task/Task.js
+++ src/main/frontend/src/components/CardDetails/Task/Task.js
@@ -1,10 +1,13 @@
 import {useState} from "react";
+import {Form} from "react-bootstrap";
 
 const Task = (props) => {
     const [task, setTask] = useState(props.task);
+    const [done, setDone] = useState(props.task.done)
     const [message, setMessage] = useState("");
 
-    const checkboxHandler = () => {
+    const doneHandler = () => {
+        setDone(!done)
         task.done = !task.done
         editTask(task)
     }
@@ -34,10 +37,9 @@ const Task = (props) => {
                 <div className="card-body">
                     <h5 className="card-title">{task.name}</h5>
                     <p className="card-text">{task.date}</p>
-                    <div className="form-group form-check m-0">
-                        <input type="checkbox" className="form-check-input" onChange={ () => checkboxHandler() } checked={task.done}/>
-                        <label className="form-check-label">Done</label>
-                    </div>
+                    <Form>
+                        <Form.Check type="switch" id={'switch' + task.id} onChange={ () => doneHandler() } checked={task.done} label={'Done'}/>
+                    </Form>
                 </div>
             </div>
         </div>
